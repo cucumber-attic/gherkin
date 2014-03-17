137acfd47a79611f2b5071199a54af2c2639eb35
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2014-03-17 22:44:22 +0000

    Extract ASTBuilder

---
+++ lib/gherkin/ast_builder.rb
@@ -0,0 +1,26 @@
+module Gherkin
+  class ASTBuilder
+
+    def initialize
+      @stack = []
+      push(:root)
+    end
+
+    def push(rule)
+      @stack.push([])
+    end
+
+    def build(token)
+      @stack.last.push(token)
+    end
+
+    def pop(rule)
+      node = @stack.pop
+      @stack.last.push(node)
+    end
+
+    def root_node?
+      @stack.first[0]
+    end
+  end
+end
