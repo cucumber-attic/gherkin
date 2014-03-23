6c77ec70985acb88b64304e2919db6e8c383fcb3
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-03-23 20:50:37 +0100

    [csharp] add simple AST builder

---
+++ Gherkin/AstNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gherkin
+{
+    public class AstNode : List<object>
+    {
+        private RuleType ruleType;
+
+        public AstNode(RuleType ruleType)
+        {
+            this.ruleType = ruleType;
+        }
+
+        public override string ToString()
+        {
+            return InternalToString("");
+        }
+
+        private string InternalToString(string indent)
+        {
+            var subIndent = indent + "\t";
+            var result = new StringBuilder();
+            result.AppendLine(indent + "[" + ruleType);
+            foreach (var subItem in this)
+                result.AppendLine(subItem is AstNode ? ((AstNode)subItem).InternalToString(subIndent) : (subIndent + subItem.ToString()));
+            result.Append(indent + "]");
+            return result.ToString();
+        }
+    }
+}
