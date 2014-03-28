4bc80e7aeff4c8af39795ae0f73d1efa5e797585
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-03-28 20:36:53 +0100

    [csharp] fix nunit version number

--- csharp/nunit.sh
+++ csharp/nunit.sh
@@ -5,7 +5,7 @@
 mono --runtime=v4.0 .nuget/NuGet.exe install NUnit.Runners -Version 2.6.3 -o packages
 
 runTest(){
-    mono --runtime=v4.0 packages/NUnit.Runners.2.6.1/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
+    mono --runtime=v4.0 packages/NUnit.Runners.2.6.3/tools/nunit-console.exe -noxml -nodots -labels -stoponerror $@
    if [ $? -ne 0 ]
    then   
      exit 1
