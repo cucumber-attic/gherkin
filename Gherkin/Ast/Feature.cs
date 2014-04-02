7a0b3ad724d1125ced1b9909efa7ed7560af0fd7
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-04-02 20:08:52 +0200

    [csharp] add new ast classes

---
+++ Gherkin/Ast/Feature.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Linq;
+
+namespace Gherkin.Ast
+{
+    public class Feature : IHasLocation, IHasDescription, IHasTags
+    {
+        public Tag[] Tags { get; private set; }
+        public Location Location { get; private set; }
+        public string Language { get; private set; }
+        public string Keyword { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public ScenarioDefinition[] ScenarioDefinitions { get; private set; }
+    }
+
+    public class Background : IHasLocation, IHasDescription, IHasSteps
+    {
+        public Location Location { get; private set; }
+        public string Keyword { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public Step[] Steps { get; private set; }
+    }
+
+    public abstract class ScenarioDefinition : IHasLocation, IHasDescription, IHasSteps, IHasTags
+    {
+        public Tag[] Tags { get; private set; }
+        public Location Location { get; private set; }
+        public string Keyword { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public Step[] Steps { get; private set; }
+    }
+
+    public class Scenario : ScenarioDefinition
+    {
+        
+    }
+
+    public class ScenarioOutline : ScenarioDefinition
+    {
+        public Examples[] Examples { get; private set; }
+    }
+
+    public class Examples : IHasLocation, IHasDescription, IHasRows, IHasTags
+    {
+        public Tag[] Tags { get; private set; }
+        public Location Location { get; private set; }
+        public string Keyword { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public TableRow Header { get; private set; }
+        public TableRow[] Rows { get; private set; }
+    }
+
+    public class Step : IHasLocation
+    {
+        public Location Location { get; private set; }
+        public string Keyword { get; private set; }
+        public string Value { get; private set; }
+        public StepArgument StepArgument { get; private set; }
+
+        public Step(string keyword, string value, StepArgument stepArgument, Location location)
+        {
+            Location = location;
+            Keyword = keyword;
+            Value = value;
+            StepArgument = stepArgument;
+        }
+    }
+
+    public abstract class StepArgument
+    {
+        
+    }
+
+    public class DataTable : StepArgument, IHasLocation, IHasRows
+    {
+        public Location Location { get; private set; }
+        public TableRow[] Rows { get; private set; }
+    }
+
+    public class DocString : StepArgument, IHasLocation
+    {
+        public Location Location { get; private set; }
+        public string ContentType { get; private set; }
+        public string Content { get; private set; }
+    }
+
+    public class EmptyStepArgument : StepArgument
+    {
+        
+    }
+
+    public class Tag : IHasLocation
+    {
+        public Location Location { get; private set; }
+        public string Value { get; private set; }
+
+        public Tag(string value, Location location)
+        {
+            Value = value;
+            Location = location;
+        }
+    }
+
+    public interface IHasTags
+    {
+        Tag[] Tags { get; }
+    }
+
+    public class TableRow : IHasLocation
+    {
+        public Location Location { get; private set; }
+        public TableCell[] Cells { get; private set; }
+
+    }
+
+    public class TableCell : IHasLocation
+    {
+        public Location Location { get; private set; }
+        public string Value { get; private set; }
+    }
+
+    public interface IHasRows
+    {
+        TableRow[] Rows { get; }
+    }
+
+    public interface IHasSteps
+    {
+        Step[] Steps { get; }
+    }
+
+    public interface IHasDescription
+    {
+        string Keyword { get; }
+        string Title { get; }
+        string Description { get; }
+    }
+
+    public interface IHasLocation
+    {
+        Location Location { get; }
+    }
+
+    public class Location
+    {
+        public string FilePath { get; private set; }
+        public int Line { get; private set; }
+        public int Column { get; private set; }
+    }
+}
