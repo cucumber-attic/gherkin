669ab5df8deb86da12dca648801f993df5a2c172
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-04-02 20:09:46 +0200

    [csharp] build Step (w/o args)

--- Gherkin.Specs/TestAstFormatter.cs
+++ Gherkin.Specs/TestAstFormatter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Gherkin.Ast;
 
 namespace Gherkin.Specs
 {
@@ -10,7 +11,26 @@ namespace Gherkin.Specs
     {
         public string FormatAst(object ast)
         {
+            if (ast is Feature)
+            {
+                var sb = new StringBuilder();
+                FormatFeature((Feature)ast, sb);
+                return sb.ToString();
+            }
+
             return ast.ToString(); //TODO
+        }
+
+        public void FormatFeature(Feature feature, StringBuilder result)
+        {
+            FormatHasDescription(feature, result);
+        }
+
+        private void FormatHasDescription(IHasDescription hasDescription, StringBuilder result)
+        {
+            result.AppendFormat("{0}: {1}", hasDescription.Keyword, hasDescription.Title);
+            if (hasDescription.Description != null)
+                result.AppendLine(hasDescription.Description);
         }
     }
 }
