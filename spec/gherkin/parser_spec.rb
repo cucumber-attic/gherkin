183bae26cd8cb95726984aebb23c8e11c9d51800
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-04-04 18:10:54 +0200

    [ruby] fix testdata path

--- spec/gherkin/parser_spec.rb
+++ spec/gherkin/parser_spec.rb
@@ -5,7 +5,7 @@ module Gherkin
   describe Parser do
     it "parses a simple feature file" do
       parser = Parser.new
-      ast = parser.parse(TokenScanner.new("../testdata/minimal.feature"))
+      ast = parser.parse(TokenScanner.new("../testdata/good/minimal.feature"))
       p ast
     end
   end
