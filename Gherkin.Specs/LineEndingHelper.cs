c69d42a8610300974be9c680a3b0e5d058d8348f
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-04-04 23:04:57 +0200

    [csharp] create tokenization test infrastructure

---
+++ Gherkin.Specs/LineEndingHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace Gherkin.Specs
+{
+	public static class LineEndingHelper
+	{
+		internal static string NormalizeLineEndings(string text)
+		{
+			return text.Replace("\r\n", "\n").TrimEnd('\n');
+		}
+	}
+}
