46855eb93692f973583ff9af5294a315b33ad2bd
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-04-04 23:37:14 +0200

    [csharp] passing tokens tests

---
+++ Gherkin.Specs/TokenFormatter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Gherkin.Specs
+{
+	public class TokenFormatter
+	{
+		public string FormatToken(Token token)
+		{
+			if (token.IsEOF)
+				return "EOF";
+
+			return string.Format("({0}:{1}){2}:{3}/{4}/{5}", 
+				token.Line.LineNumber + 1,
+				token.Indent + 1,
+				token.MatchedType,
+				token.MatchedKeyword,
+				token.Text,
+				token.Items == null ? "" : string.Join(",", token.Items)
+				);
+		}
+	}
+}
