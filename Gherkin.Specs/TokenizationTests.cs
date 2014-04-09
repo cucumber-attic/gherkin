3dbafdfa765f1628376d01b6cb161a5e18164b81
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-04-09 11:42:29 +0200

    [csharp] add tokens tester

--- Gherkin.Specs/TokenizationTests.cs
+++ Gherkin.Specs/TokenizationTests.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using NUnit.Framework;
 
 namespace Gherkin.Specs
@@ -30,37 +29,6 @@ namespace Gherkin.Specs
 			var expectedTokensText = LineEndingHelper.NormalizeLineEndings(File.ReadAllText(expectedTokensFile));
 
 			Assert.AreEqual(expectedTokensText, tokensText);
-		}
-
-		class TokenFormatterBuilder : IAstBuilder
-		{
-			private readonly TokenFormatter formatter = new TokenFormatter();
-			private readonly StringBuilder tokensTextBuilder = new StringBuilder();
-
-			public string GetTokensText()
-			{
-				return LineEndingHelper.NormalizeLineEndings(tokensTextBuilder.ToString());
-			}
-
-			public void Build(Token token)
-			{
-				tokensTextBuilder.AppendLine(formatter.FormatToken(token));
-			}
-
-			public void StartRule(RuleType ruleType)
-			{
-				//nop
-			}
-
-			public void EndRule(RuleType ruleType)
-			{
-				//nop
-			}
-
-			public object GetResult()
-			{
-				return new object();
-			}
 		}
 	}
 }
---
+++ Gherkin.TokensTester/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Gherkin.Specs;
+
+namespace Gherkin.TokensTester
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("Usage: Gherkin.TokenTester.exe test-feature-file.feature");
+				return 100;
+			}
+
+			string featureFilePath = args[0];
+
+			return TestTokens(featureFilePath);
+		}
+
+		private static int TestTokens(string featureFilePath)
+		{
+			try
+			{
+				return TestTokensInternal(featureFilePath);
+			}
+			catch (Exception ex)
+			{
+				Console.Error.WriteLine(ex.Message);
+				return 1;
+			}
+		}
+
+		private static int TestTokensInternal(string featureFilePath)
+		{
+			var parser = new Parser();
+			var tokenFormatterBuilder = new TokenFormatterBuilder();
+			using (var reader = new StreamReader(featureFilePath))
+				parser.Parse(new TokenScanner(reader), new TokenMatcher(), tokenFormatterBuilder);
+
+			var tokensText = tokenFormatterBuilder.GetTokensText();
+			Console.WriteLine(tokensText);
+			return 0;
+		}
+	}
+}
