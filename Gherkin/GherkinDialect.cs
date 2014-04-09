c221fa416709b5ed73dffd3b6899ed03da9f11be
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-04-09 12:57:32 +0200

    [csharp] refactor gherkin dialect

--- Gherkin/GherkinDialect.cs
+++ Gherkin/GherkinDialect.cs
@@ -5,20 +5,24 @@ namespace Gherkin
 {
     public class GherkinDialect
     {
-        private readonly string[] featureKeywords;
-        private readonly string[] backgroundKeywords;
-        private readonly string[] scenarioKeywords;
-        private readonly string[] scenarioOutlineKeywords;
-        private readonly string[] examplesKeywords;
-        private readonly string[] givenStepKeywords;
-        private readonly string[] whenStepKeywords;
-        private readonly string[] thenStepKeywords;
-        private readonly string[] andStepKeywords;
-        private readonly string[] butStepKeywords;
+		public string Language { get; private set; }
 
-        private readonly string[] stepKeywords;
+		public string[] FeatureKeywords { get; private set; }
+		public string[] BackgroundKeywords { get; private set; }
+		public string[] ScenarioKeywords { get; private set; }
+		public string[] ScenarioOutlineKeywords { get; private set; }
+		public string[] ExamplesKeywords { get; private set; }
+		public string[] GivenStepKeywords { get; private set; }
+		public string[] WhenStepKeywords { get; private set; }
+		public string[] ThenStepKeywords { get; private set; }
+		public string[] AndStepKeywords { get; private set; }
+		public string[] ButStepKeywords { get; private set; }
 
-        public GherkinDialect(
+
+		public string[] StepKeywords { get; private set; }
+
+	    public GherkinDialect(
+			string language,
             string[] featureKeywords, 
             string[] backgroundKeywords, 
             string[] scenarioKeywords,
@@ -30,96 +34,24 @@ namespace Gherkin
             string[] andStepKeywords,
             string[] butStepKeywords)
         {
-            this.featureKeywords = featureKeywords;
-            this.backgroundKeywords = backgroundKeywords;
-            this.scenarioKeywords = scenarioKeywords;
-            this.scenarioOutlineKeywords = scenarioOutlineKeywords;
-            this.examplesKeywords = examplesKeywords;
-            this.givenStepKeywords = givenStepKeywords;
-            this.whenStepKeywords = whenStepKeywords;
-            this.thenStepKeywords = thenStepKeywords;
-            this.andStepKeywords = andStepKeywords;
-            this.butStepKeywords = butStepKeywords;
+		    Language = language;
+		    FeatureKeywords = featureKeywords;
+            BackgroundKeywords = backgroundKeywords;
+            ScenarioKeywords = scenarioKeywords;
+            ScenarioOutlineKeywords = scenarioOutlineKeywords;
+            ExamplesKeywords = examplesKeywords;
+            GivenStepKeywords = givenStepKeywords;
+            WhenStepKeywords = whenStepKeywords;
+            ThenStepKeywords = thenStepKeywords;
+            AndStepKeywords = andStepKeywords;
+            ButStepKeywords = butStepKeywords;
 
-            this.stepKeywords = givenStepKeywords
+            StepKeywords = givenStepKeywords
                 .Concat(whenStepKeywords)
                 .Concat(thenStepKeywords)
                 .Concat(andStepKeywords)
                 .Concat(butStepKeywords)
                 .ToArray();
-        }
-
-        public string[] FeatureKeywords
-        {
-            get { return featureKeywords; }
-        }
-
-        public string[] BackgroundKeywords
-        {
-            get { return backgroundKeywords; }
-        }
-
-        public string[] ScenarioKeywords
-        {
-            get { return scenarioKeywords; }
-        }
-
-        public string[] ScenarioOutlineKeywords
-        {
-            get { return scenarioOutlineKeywords; }
-        }
-
-        public string[] ExamplesKeywords
-        {
-            get { return examplesKeywords; }
-        }
-
-        public string[] StepKeywords
-        {
-            get { return stepKeywords; }
-        }
-
-        public string[] GivenStepKeywords
-        {
-            get { return givenStepKeywords; }
-        }
-
-        public string[] WhenStepKeywords
-        {
-            get { return whenStepKeywords; }
-        }
-
-        public string[] ThenStepKeywords
-        {
-            get { return thenStepKeywords; }
-        }
-
-        public string[] AndStepKeywords
-        {
-            get { return andStepKeywords; }
-        }
-
-        public string[] ButStepKeywords
-        {
-            get { return butStepKeywords; }
-        }
-
-        public string[] GetTitleKeywords(TokenType tokenType)
-        {
-            switch (tokenType)
-            {
-                case TokenType.FeatureLine:
-                    return FeatureKeywords;
-                case TokenType.BackgroundLine:
-                    return BackgroundKeywords;
-                case TokenType.ScenarioLine:
-                    return ScenarioKeywords;
-                case TokenType.ScenarioOutlineLine:
-                    return ScenarioOutlineKeywords;
-                case TokenType.ExamplesLine:
-                    return ExamplesKeywords;
-            }
-            throw new NotSupportedException();
         }
     }
 }
