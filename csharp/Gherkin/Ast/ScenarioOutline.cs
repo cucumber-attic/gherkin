235d511e524b546fac2c5500755c11ebcf784c36
Gaspar Nagy <gaspar.nagy@gmail.com>
2014-04-09 15:07:51 +0200

    [csharp] small AST refactoring

--- csharp/Gherkin/Ast/ScenarioOutline.cs
+++ csharp/Gherkin/Ast/ScenarioOutline.cs
@@ -7,7 +7,8 @@ namespace Gherkin.Ast
 	{
 		public Examples[] Examples { get; private set; }
 
-		public ScenarioOutline(Tag[] tags, Location location, string keyword, string title, string description, Step[] steps, Examples[] examples) : base(tags, location, keyword, title, description, steps)
+		public ScenarioOutline(Tag[] tags, Location location, string keyword, string title, string description, Step[] steps, Examples[] examples) 
+			: base(tags, location, keyword, title, description, steps)
 		{
 			Examples = examples;
 		}
