a5de6b83d24e559ea12e3f142c0c6f7debe1401b
Steve Tooke <steve@boxjump.co.uk>
2015-02-27 14:55:33 +0000

    Keep exceptions simple

--- ruby/lib/gherkin/errors.rb
+++ ruby/lib/gherkin/errors.rb
@@ -1,16 +1,17 @@
 module Gherkin
   class ParserException < StandardError
     def initialize(message, location)
-      super(get_message(message, location))
-      @location = location
-    end
-
-    private
-    def get_message(message, location)
-      "(#{location.line}:#{location.column}): #{message}"
+      super("(#{location.line}:#{location.column}): #{message}")
     end
   end
 
   class CompositeParserException < ParserException
+  end
+
+  class UnexpectedTokenException < ParserException
+    def initialize(received_token, expected_token_types, state_comment)
+      message = "expected: #{expected_token_types.join(", ")}, got '#{received_token.get_token_value.strip}'"
+      super(message, received_token.location)
+    end
   end
 end
