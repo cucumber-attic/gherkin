f7e595878e02f220f6051776a2ecad006b5992d2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-03-05 18:26:11 +0000

    Started work on js compiler

---
+++ test/compiler_test.js
@@ -0,0 +1,32 @@
+var assert = require('assert');
+var Compiler = require('../lib/gherkin/compiler');
+
+describe('Compiler', function () {
+  var compile;
+
+  beforeEach(function () {
+    var compiler = new Compiler();
+    var result = [];
+    compiler.on('test-case', function (testCase) {
+      result.push({type: 'TestCase', value: testCase});
+    });
+    compiler.on('test-step', function (testStep) {
+      result.push({type: 'TestStep', value: testStep});
+    });
+
+    compile = function(path) {
+      var feature = require(path);
+      compiler.compile(feature);
+      return result;
+    }
+  });
+
+  it("compiles a feature with a single scenario", function () {
+    var expected = [
+      { type: 'TestCase', value: { location: { column: 3, line: 3 } } },
+      { type: 'TestStep', value: { location: { column: 5, line: 4 } } }
+    ];
+
+    assert.deepEqual(compile('../../testdata/good/minimal.feature.ast.json'), expected);
+  });
+});
