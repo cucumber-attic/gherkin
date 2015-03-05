23b31c66ffdc278d6f786c2a92c9fdda0968feb2
Steve Tooke <steve@boxjump.co.uk>
2015-03-05 13:48:15 +0000

    Simple token skeleton

--- ruby/lib/gherkin/token.rb
+++ ruby/lib/gherkin/token.rb
@@ -1,4 +1,13 @@
 module Gherkin
   class Token
+    attr_reader :line
+    def initialize(line, location)
+      @line = line
+      @location = location
+    end
+
+    def eof?
+      @line.nil?
+    end
   end
 end
