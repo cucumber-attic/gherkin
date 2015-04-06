f53d28e92512dd1648faf0b9e196ef622ede9ef2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-04-06 15:19:34 +0100

    Implementations have moved to separate repos

--- java/src/main/java/gherkin/IGherkinDialectProvider.java
+++
@@ -1,9 +0,0 @@
-package gherkin;
-
-import gherkin.ast.Location;
-
-public interface IGherkinDialectProvider {
-    public GherkinDialect getDefaultDialect();
-
-    GherkinDialect getDialect(String language, Location location);
-}
--- ruby/spec/gherkin/parser_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'gherkin/parser'
-require 'gherkin/token_scanner'
-
-module Gherkin
-  describe Parser do
-  end
-end
