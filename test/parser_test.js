8810e77e930f61a08294641df7a08c242e3e3d22
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-04-06 18:18:06 +0100

    Simple parser test

---
+++ test/parser_test.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var Gherkin = require('..');
+
+describe('Parser', function () {
+  it("parses a simple feature", function () {
+    var parser = new Gherkin.Parser();
+    var scanner = new Gherkin.TokenScanner("Feature: hello");
+    var builder = new Gherkin.AstBuilder();
+    var ast = parser.parse(scanner, builder, new Gherkin.TokenMatcher());
+    assert.deepEqual(ast, {
+      type: 'Feature',
+      tags: [],
+      location: { line: 1, column: 1 },
+      language: 'en',
+      keyword: 'Feature',
+      name: 'hello',
+      description: undefined,
+      background: undefined,
+      scenarioDefinitions: []
+    });
+  });
+});
