6862492485b7716bc2fa15f058b36bbb821d074a
Gaspar Nagy <gaspar.nagy@gmail.com>
2015-04-08 15:10:17 +0200

    use header and body instead of rows in Examples (c#)

--- dotnet/Gherkin/Ast/Examples.cs
+++ dotnet/Gherkin/Ast/Examples.cs
@@ -12,18 +12,22 @@ namespace Gherkin.Ast
         public string Name { get; private set; }
         public string Description { get; private set; }
         public TableRow Header { get; private set; }
-        public IEnumerable<TableRow> Rows { get; private set; }
+        public IEnumerable<TableRow> Body { get; private set; }
 
-        public Examples(Tag[] tags, Location location, string keyword, string name, string description, TableRow header, TableRow[] rows)
+        public Examples(Tag[] tags, Location location, string keyword, string name, string description, TableRow header, TableRow[] body)
         {
             Tags = tags;
             Location = location;
             Keyword = keyword;
             Name = name;
             Description = description;
-            //TODO: fix Examples header/rows handling properly
-            Header = null;
-            Rows = new TableRow[] {header}.Concat(rows);
+            Header = header;
+            Body = body;
+        }
+
+        IEnumerable<TableRow> IHasRows.Rows
+        {
+            get { return new TableRow[] {Header}.Concat(Body); }
         }
     }
 }
