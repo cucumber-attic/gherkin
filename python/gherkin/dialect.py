1c82314db4460d490d577338f68c28b031a0973b
Björn Rasmusson <B.Rasmusson@computer.org>
2015-07-26 13:31:35 +0200

    Add the python language
    Works for both python2 (2.7) and python3 (3.4).

---
+++ python/gherkin/dialect.py
@@ -0,0 +1,44 @@
+import os
+import json
+
+
+class Dialect:
+    DIALECTS = json.load(open(os.path.join(os.path.dirname(__file__),
+                                           'gherkin-languages.json'), 'r'), encoding='utf-8')
+
+    @classmethod
+    def for_name(cls, name):
+        return cls(cls.DIALECTS[name]) if name in cls.DIALECTS else None
+
+    def __init__(self, spec):
+        self.spec = spec
+
+    def feature_keywords(self):
+        return self.spec['feature']
+
+    def scenario_keywords(self):
+        return self.spec['scenario']
+
+    def scenario_outline_keywords(self):
+        return self.spec['scenarioOutline']
+
+    def background_keywords(self):
+        return self.spec['background']
+
+    def examples_keywords(self):
+        return self.spec['examples']
+
+    def given_keywords(self):
+        return self.spec['given']
+
+    def when_keywords(self):
+        return self.spec['when']
+
+    def then_keywords(self):
+        return self.spec['then']
+
+    def and_keywords(self):
+        return self.spec['and']
+
+    def but_keywords(self):
+        return self.spec['but']
