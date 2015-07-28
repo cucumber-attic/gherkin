00c3b508280017c38ad2d8d37d5966687c6705da
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-07-28 16:10:41 -0500

    Added @brasmusson's Python parser
    Squashed commit of the following:
    
    commit b884b1005bae189f0962eb08d9add18287f38864
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Tue Jul 28 16:08:32 2015 -0500
    
        Add Python example
    
    commit 3ad60cd8d170848ec393b577cdbd993624b47701
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Tue Jul 28 16:01:15 2015 -0500
    
        Fix environment variable check
    
    commit 2dd0a54742da3b036792afd24b940391f186c72e
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Tue Jul 28 15:46:42 2015 -0500
    
        Make ./perf work with Python
    
    commit a089907dc76738ed07304cd12a3367e1004425d7
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Tue Jul 28 15:32:46 2015 -0500
    
        Adjust path relative to file, not PWD
    
    commit ae63305f7c9709036456752d254b1c8e39a138b7
    Author: Björn Rasmusson <B.Rasmusson@computer.org>
    Date:   Tue Jul 28 19:11:23 2015 +0200
    
        Build the branch add-python-language on Travis
    
    commit 1c82314db4460d490d577338f68c28b031a0973b
    Author: Björn Rasmusson <B.Rasmusson@computer.org>
    Date:   Sun Jul 26 13:31:35 2015 +0200
    
        Add the python language
    
        Works for both python2 (2.7) and python3 (3.4).

---
+++ gherkin/gherkin_line.py
@@ -0,0 +1,43 @@
+class GherkinLine:
+    def __init__(self, line_text, line_number):
+        self._line_text = line_text
+        self._line_number = line_number
+        self._trimmed_line_text = line_text.lstrip()
+        self.indent = len(line_text) - len(self._trimmed_line_text)
+
+    def get_rest_trimmed(self, length):
+        return self._trimmed_line_text[length:].strip()
+
+    def get_line_text(self, indent_to_remove=-1):
+        if indent_to_remove < 0 or indent_to_remove > self.indent:
+            return self._trimmed_line_text
+        else:
+            return self._line_text[indent_to_remove:]
+
+    def is_empty(self):
+        return not self._trimmed_line_text
+
+    def startswith(self, prefix):
+        return self._trimmed_line_text.startswith(prefix)
+
+    def startswith_title_keyword(self, keyword):
+        return self._trimmed_line_text.startswith(keyword + ':')
+
+    def table_cells(self):
+        column = self.indent + 1
+        items = self._trimmed_line_text.strip().split('|')
+        cells = []
+        for item in items[1:-1]:
+            cell_indent = len(item) - len(item.lstrip()) + 1
+            cells.append({'column': column + cell_indent, 'text': item.strip()})
+            column += len(item) + 1
+        return cells
+
+    def tags(self):
+        column = self.indent + 1
+        items = self._trimmed_line_text.strip().split('@')
+        tags = []
+        for item in items[1:]:
+            tags.append({'column': column, 'text': '@' + item.strip()})
+            column += len(item) + 1
+        return tags
---
+++ gherkin/token_scanner.py
@@ -0,0 +1,27 @@
+import os
+from .token import Token
+from .gherkin_line import GherkinLine
+try:
+    python2 = True
+    from cStringIO import StringIO
+except ImportError:
+    python2 = False
+    from io import StringIO
+
+
+class TokenScanner:
+    def __init__(self, io):
+        if isinstance(io, str):
+            if os.path.exists(io):
+                self.io = open(io, 'rU')
+            else:
+                self.io = StringIO(io)
+        self.line_number = 0
+
+    def read(self):
+        self.line_number += 1
+        location = {'line': self.line_number}
+        line = self.io.readline()
+        if python2:
+            line = line.decode('utf-8')
+        return Token((GherkinLine(line, self.line_number) if line else line), location)
