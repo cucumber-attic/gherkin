9b314c36f7dff7c58f6cc58faa822aa4941c64a2
Alexey Kotlyarov <a@koterpillar.com>
2015-08-20 20:06:36 +1000

    Java: handle escaping in table cells

--- src/main/java/gherkin/GherkinLine.java
+++ src/main/java/gherkin/GherkinLine.java
@@ -64,7 +64,42 @@ public class GherkinLine implements IGherkinLine {
 
     @Override
     public List<GherkinLineSpan> getTableCells() {
-        return getSpans("\\s*\\|\\s*");
+        List<GherkinLineSpan> lineSpans = new ArrayList<GherkinLineSpan>();
+        StringBuilder cell = new StringBuilder();
+        boolean beforeFirst = true;
+        int startCol = 0;
+        for (int col = 0; col < trimmedLineText.length(); col++) {
+            char c = trimmedLineText.charAt(col);
+            if (c == '|') {
+                if (beforeFirst) {
+                    // Skip the first empty span
+                    beforeFirst = false;
+                } else {
+                    int contentStart = 0;
+                    while (contentStart < cell.length() && Character.isWhitespace(cell.charAt(contentStart))) {
+                        contentStart++;
+                    }
+                    if (contentStart == cell.length()) {
+                        contentStart = 0;
+                    }
+                    lineSpans.add(new GherkinLineSpan(indent() + startCol + contentStart + 2, cell.toString().trim()));
+                    startCol = col;
+                }
+                cell = new StringBuilder();
+            } else if (c == '\\') {
+                col++;
+                c = trimmedLineText.charAt(col);
+                if (c == 'n') {
+                    cell.append('\n');
+                } else {
+                    cell.append(c);
+                }
+            } else {
+                cell.append(c);
+            }
+        }
+
+        return lineSpans;
     }
 
     private List<GherkinLineSpan> getSpans(String delimiter) {
