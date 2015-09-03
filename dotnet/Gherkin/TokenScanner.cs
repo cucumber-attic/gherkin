a781fcb7f7d53e987df5e0df065dd45f7e6bbf07
Zearin <tgoodchild@gmail.com>
2015-09-03 10:02:37 -0400

    Added short description comments for TokenScanner
    * The description is taken from the README.md
    * I looked up the basic rules for “documentation comments” for each language, in order to provide (minimal but functional) support for documentation generators

--- dotnet/Gherkin/TokenScanner.cs
+++ dotnet/Gherkin/TokenScanner.cs
@@ -5,6 +5,16 @@ using Gherkin.Ast;
 
 namespace Gherkin
 {
+    /// <summary>
+    /// The scanner reads a gherkin doc (typically read from a .feature file) and creates a token 
+    /// for each line. 
+    /// 
+    /// The tokens are passed to the parser, which outputs an AST (Abstract Syntax Tree).
+    /// 
+    /// If the scanner sees a `#` language header, it will reconfigure itself dynamically to look 
+    /// for  Gherkin keywords for the associated language. The keywords are defined in 
+    /// gherkin-languages.json.
+    /// </summary>
     public class TokenScanner : ITokenScanner
     {
         protected int lineNumber = 0;
--- javascript/lib/gherkin/token_scanner.js
+++ javascript/lib/gherkin/token_scanner.js
@@ -1,6 +1,13 @@
 var Token = require('./token');
 var GherkinLine = require('./gherkin_line');
 
+/**
+ * The scanner reads a gherkin doc (typically read from a .feature file) and creates a token for each line. 
+ * The tokens are passed to the parser, which outputs an AST (Abstract Syntax Tree).
+ * 
+ * If the scanner sees a `#` language header, it will reconfigure itself dynamically to look for 
+ * Gherkin keywords for the associated language. The keywords are defined in gherkin-languages.json.
+ */
 module.exports = function TokenScanner(source) {
   var lines = source.split(/\r?\n/);
   if(lines.length > 0 && lines[lines.length-1].trim() == '') {
