c9b2ebb4fa71195b95d8283249bbec2dc3bdeb19
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-09-24 18:50:22 +0100

    Fix failing test. Give cred.

--- lib/gherkin3/token_scanner.rb
+++ lib/gherkin3/token_scanner.rb
@@ -1,24 +1,26 @@
+require 'stringio'
 require_relative 'token'
 require_relative 'gherkin_line'
 
 module Gherkin3
-  # The scanner reads a gherkin doc (typically read from a .feature file) and 
-  # creates a token for line. The tokens are passed to the parser, which outputs 
+  # The scanner reads a gherkin doc (typically read from a .feature file) and
+  # creates a token for line. The tokens are passed to the parser, which outputs
   # an AST (Abstract Syntax Tree).
   #
-  # If the scanner sees a # language header, it will reconfigure itself dynamically 
-  # to look for Gherkin keywords for the associated language. The keywords are defined 
+  # If the scanner sees a # language header, it will reconfigure itself dynamically
+  # to look for Gherkin keywords for the associated language. The keywords are defined
   # in gherkin-languages.json.
   class TokenScanner
     def initialize(source_or_io)
       @line_number = 0
 
-      if String === source_or_io
+      case(source_or_io)
+      when String
         @io = StringIO.new(source_or_io)
-      elsif source_or_io.is_a? IO
+      when StringIO, IO
         @io = source_or_io
       else
-        fail ArgumentError, 'Please a pass "String" or "IO".'
+        fail ArgumentError, "Please a pass String, StringIO or IO. I got a #{source_or_io.class}"
       end
     end
 
