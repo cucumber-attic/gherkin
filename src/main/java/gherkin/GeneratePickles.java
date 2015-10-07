b7c64e0878c98b59639b6061ca0b22168ae7d133
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-10-07 21:06:43 +0100

    Compiler (currently implemented in Java and JavaScript)
    Squashed commit of the following:
    
    commit d1dd8cc285afe5cf0f1c8140020635a250e3539c
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Wed Oct 7 21:04:47 2015 +0100
    
        Remove meaningless junit tests
    
    commit cb821057927f29e9dadbbac62cfc540205096723
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Wed Oct 7 20:59:15 2015 +0100
    
        Fix broken test
    
    commit 9091462030e30ac35e3052d82151bc851d16258d
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Sat Oct 3 16:31:11 2015 +0100
    
        Consistently behaving compilers in Java and JavaScript
    
    commit eed2dd351563f0e4434804ea553ced797a18dabd
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Sun Sep 27 22:10:50 2015 +0100
    
        Compiler (mostly) implemented in Java.
    
        To simplify the design I decided to reuse classes from the Ast package.
        That exposed a subtle difference - the test cases (and arguments) need
        a list of locations, so reusing the same DocString and DataTable classes
        is probably a bad idea.
    
        I'll probably reintroduce the Pickles. I thought for a while that it
        would be simpler to have TestCases in Gherkin, and let Cucumber simply
        create derived TestCases with extra Before/After hooks, and also
        "linking" step definitions.
    
        It's become clear to me that it's better to differentiate between those
        bounded contexts: compilation and execution.
    
        Next step will be to reintroduce pickles. Cucumber can create TestCases
        from those. They'll be similar, but slightly different (they might have
        slots for linking stepdefs).
    
    commit 1af0eb9413f163079ef88e04d250cbb9bfad2afa
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Mon Sep 14 08:15:59 2015 +0100
    
        Add paths to testcases
    
    commit dfcc6278d0912a2858e8df1435b7f56741d7c298
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Sat Sep 12 14:57:48 2015 +0100
    
        Compile tags
    
    commit 566062e8a21ecc2cf2bdd041e3e61f3837adef9f
    Author: Aslak Hellesøy <aslak.hellesoy@gmail.com>
    Date:   Fri Sep 11 19:01:28 2015 +0100
    
        Better JavaScript compiler

--- src/main/java/gherkin/GeneratePickles.java
+++ src/main/java/gherkin/GeneratePickles.java
@@ -1,9 +1,10 @@
 package gherkin;
 
 import gherkin.ast.Feature;
-import gherkin.compiler.Compiler;
+import gherkin.pickles.Compiler;
 import gherkin.deps.com.google.gson.Gson;
-import pickles.Pickle;
+import gherkin.pickles.Pickle;
+import gherkin.deps.com.google.gson.GsonBuilder;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,7 +14,7 @@ import java.util.List;
 
 public class GeneratePickles {
     public static void main(String[] args) throws IOException {
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         Parser<Feature> parser = new Parser<>(new AstBuilder());
         TokenMatcher matcher = new TokenMatcher();
         Compiler compiler = new Compiler();
@@ -23,7 +24,7 @@ public class GeneratePickles {
             InputStreamReader in = new InputStreamReader(new FileInputStream(fileName), "UTF-8");
             try {
                 Feature feature = parser.parse(in, matcher);
-                pickles.addAll(compiler.compile(feature));
+                pickles.addAll(compiler.compile(feature, fileName));
             } catch (ParserException e) {
                 System.err.println(e.getMessage());
                 System.exit(1);
