56078432debeefd23a824b9e4c7ea3b8b3bc9b4a
LiohAu <julien.curro@gmail.com>
2015-10-15 23:17:09 +0200

    Adding TokensGenerator application and modifying Makefile consequently

---
+++ TokensGenerator/GHTestTokenFormatter.m
@@ -0,0 +1,13 @@
+//
+//  GHTestTokenFormatter.m
+//  Gherkin
+//
+//  Created by Julien Curro on 15/10/15.
+//  Copyright © 2015 Gherkin. All rights reserved.
+//
+
+#import "GHTestTokenFormatter.h"
+
+@implementation GHTestTokenFormatter
+
+@end
