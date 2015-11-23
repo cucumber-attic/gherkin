d71d29b127da7ae5f0d3d2830d4e178aec019fe3
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-11-23 23:52:41 +0000

    Add Emoji translation of Gherkin...
    I thought this was going to be a simple, innocuous change, but
    I quickly discovered how several platforms' strings are broken
    for characters outside the Basic Multilingual Plane (BMP).
    
    The locations are now portable, and columns now represent the
    position of the Unicode _code point_.

--- dialects_builtin.go
+++ dialects_builtin.go
@@ -1,6 +1,6 @@
 package gherkin
 
-// Builtin dialects for af (Afrikaans), am (Armenian), ar (Arabic), bg (Bulgarian), bm (Malay), bs (Bosnian), ca (Catalan), cs (Czech), cy-GB (Welsh), da (Danish), de (German), el (Greek), en (English), en-Scouse (Scouse), en-au (Australian), en-lol (LOLCAT), en-old (Old English), en-pirate (Pirate), eo (Esperanto), es (Spanish), et (Estonian), fa (Persian), fi (Finnish), fr (French), ga (Irish), gj (Gujarati), gl (Galician), he (Hebrew), hi (Hindi), hr (Croatian), ht (Creole), hu (Hungarian), id (Indonesian), is (Icelandic), it (Italian), ja (Japanese), jv (Javanese), kn (Kannada), ko (Korean), lt (Lithuanian), lu (Luxemburgish), lv (Latvian), nl (Dutch), no (Norwegian), pa (Panjabi), pl (Polish), pt (Portuguese), ro (Romanian), ru (Russian), sk (Slovak), sl (Slovenian), sr-Cyrl (Serbian), sr-Latn (Serbian (Latin)), sv (Swedish), ta (Tamil), th (Thai), tl (Telugu), tlh (Klingon), tr (Turkish), tt (Tatar), uk (Ukrainian), ur (Urdu), uz (Uzbek), vi (Vietnamese), zh-CN (Chinese simplified), zh-TW (Chinese traditional)
+// Builtin dialects for af (Afrikaans), am (Armenian), ar (Arabic), bg (Bulgarian), bm (Malay), bs (Bosnian), ca (Catalan), cs (Czech), cy-GB (Welsh), da (Danish), de (German), el (Greek), em (Emoji), en (English), en-Scouse (Scouse), en-au (Australian), en-lol (LOLCAT), en-old (Old English), en-pirate (Pirate), eo (Esperanto), es (Spanish), et (Estonian), fa (Persian), fi (Finnish), fr (French), ga (Irish), gj (Gujarati), gl (Galician), he (Hebrew), hi (Hindi), hr (Croatian), ht (Creole), hu (Hungarian), id (Indonesian), is (Icelandic), it (Italian), ja (Japanese), jv (Javanese), kn (Kannada), ko (Korean), lt (Lithuanian), lu (Luxemburgish), lv (Latvian), nl (Dutch), no (Norwegian), pa (Panjabi), pl (Polish), pt (Portuguese), ro (Romanian), ru (Russian), sk (Slovak), sl (Slovenian), sr-Cyrl (Serbian), sr-Latn (Serbian (Latin)), sv (Swedish), ta (Tamil), th (Thai), tl (Telugu), tlh (Klingon), tr (Turkish), tt (Tatar), uk (Ukrainian), ur (Urdu), uz (Uzbek), vi (Vietnamese), zh-CN (Chinese simplified), zh-TW (Chinese traditional)
 func GherkinDialectsBuildin() GherkinDialectProvider {
 	return buildinDialects
 }
@@ -515,6 +515,45 @@ var buildinDialects = gherkinDialectMap{
 			when: []string{
 				"* ",
 				"Όταν ",
+			},
+		},
+	},
+	"em": &GherkinDialect{
+		"em", "Emoji", "😀", map[string][]string{
+			and: []string{
+				"* ",
+				"😂",
+			},
+			background: []string{
+				"💤",
+			},
+			but: []string{
+				"* ",
+				"😔",
+			},
+			examples: []string{
+				"📓",
+			},
+			feature: []string{
+				"📚",
+			},
+			given: []string{
+				"* ",
+				"😐",
+			},
+			scenario: []string{
+				"📕",
+			},
+			scenarioOutline: []string{
+				"📖",
+			},
+			then: []string{
+				"* ",
+				"🙏",
+			},
+			when: []string{
+				"* ",
+				"🎬",
 			},
 		},
 	},
