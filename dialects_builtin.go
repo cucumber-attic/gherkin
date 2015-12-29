1adb373597c7fd8c3954bc5302c815d93a567d08
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2015-12-29 00:46:12 -0600

    Attribution for Mongolian, ref #140

--- dialects_builtin.go
+++ dialects_builtin.go
@@ -1,6 +1,6 @@
 package gherkin
 
-// Builtin dialects for af (Afrikaans), am (Armenian), ar (Arabic), bg (Bulgarian), bm (Malay), bs (Bosnian), ca (Catalan), cs (Czech), cy-GB (Welsh), da (Danish), de (German), el (Greek), em (Emoji), en (English), en-Scouse (Scouse), en-au (Australian), en-lol (LOLCAT), en-old (Old English), en-pirate (Pirate), eo (Esperanto), es (Spanish), et (Estonian), fa (Persian), fi (Finnish), fr (French), ga (Irish), gj (Gujarati), gl (Galician), he (Hebrew), hi (Hindi), hr (Croatian), ht (Creole), hu (Hungarian), id (Indonesian), is (Icelandic), it (Italian), ja (Japanese), jv (Javanese), kn (Kannada), ko (Korean), lt (Lithuanian), lu (Luxemburgish), lv (Latvian), nl (Dutch), no (Norwegian), pa (Panjabi), pl (Polish), pt (Portuguese), ro (Romanian), ru (Russian), sk (Slovak), sl (Slovenian), sr-Cyrl (Serbian), sr-Latn (Serbian (Latin)), sv (Swedish), ta (Tamil), th (Thai), tl (Telugu), tlh (Klingon), tr (Turkish), tt (Tatar), uk (Ukrainian), ur (Urdu), uz (Uzbek), vi (Vietnamese), zh-CN (Chinese simplified), zh-TW (Chinese traditional)
+// Builtin dialects for af (Afrikaans), am (Armenian), ar (Arabic), bg (Bulgarian), bm (Malay), bs (Bosnian), ca (Catalan), cs (Czech), cy-GB (Welsh), da (Danish), de (German), el (Greek), em (Emoji), en (English), en-Scouse (Scouse), en-au (Australian), en-lol (LOLCAT), en-old (Old English), en-pirate (Pirate), eo (Esperanto), es (Spanish), et (Estonian), fa (Persian), fi (Finnish), fr (French), ga (Irish), gj (Gujarati), gl (Galician), he (Hebrew), hi (Hindi), hr (Croatian), ht (Creole), hu (Hungarian), id (Indonesian), is (Icelandic), it (Italian), ja (Japanese), jv (Javanese), kn (Kannada), ko (Korean), lt (Lithuanian), lu (Luxemburgish), lv (Latvian), mn (Mongolian), nl (Dutch), no (Norwegian), pa (Panjabi), pl (Polish), pt (Portuguese), ro (Romanian), ru (Russian), sk (Slovak), sl (Slovenian), sr-Cyrl (Serbian), sr-Latn (Serbian (Latin)), sv (Swedish), ta (Tamil), th (Thai), tl (Telugu), tlh (Klingon), tr (Turkish), tt (Tatar), uk (Ukrainian), ur (Urdu), uz (Uzbek), vi (Vietnamese), zh-CN (Chinese simplified), zh-TW (Chinese traditional)
 func GherkinDialectsBuildin() GherkinDialectProvider {
 	return buildinDialects
 }
@@ -1847,6 +1847,50 @@ var buildinDialects = gherkinDialectMap{
 			when: []string{
 				"* ",
 				"Ja ",
+			},
+		},
+	},
+	"mn": &GherkinDialect{
+		"mn", "Mongolian", "монгол", map[string][]string{
+			and: []string{
+				"* ",
+				"Мөн ",
+				"Тэгээд ",
+			},
+			background: []string{
+				"Агуулга",
+			},
+			but: []string{
+				"* ",
+				"Гэхдээ ",
+				"Харин ",
+			},
+			examples: []string{
+				"Тухайлбал",
+			},
+			feature: []string{
+				"Функц",
+				"Функционал",
+			},
+			given: []string{
+				"* ",
+				"Өгөгдсөн нь ",
+				"Анх ",
+			},
+			scenario: []string{
+				"Сценар",
+			},
+			scenarioOutline: []string{
+				"Сценарын төлөвлөгөө",
+			},
+			then: []string{
+				"* ",
+				"Тэгэхэд ",
+				"Үүний дараа ",
+			},
+			when: []string{
+				"* ",
+				"Хэрэв ",
 			},
 		},
 	},
