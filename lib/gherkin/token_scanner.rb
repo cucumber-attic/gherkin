4eb2c3d39dff27bc7605bc75d9685e8f4afa8acb
Maxime Garcia <maxime.garcia@gmail.com>
2016-03-13 15:10:14 +0100

    Use require instead of require_relative

--- lib/gherkin/token_scanner.rb
+++ lib/gherkin/token_scanner.rb
@@ -1,6 +1,6 @@
 require 'stringio'
-require_relative 'token'
-require_relative 'gherkin_line'
+require 'gherkin/token'
+require 'gherkin/gherkin_line'
 
 module Gherkin
   # The scanner reads a gherkin doc (typically read from a .feature file) and
