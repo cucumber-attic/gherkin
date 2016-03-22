0e2ffca9cad9a01d6cf8b609d8d2680895d85eeb
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2016-03-22 23:39:36 +0000

    Fix go tests

--- go/example_test.go
+++ go/example_test.go
@@ -38,15 +38,15 @@ func ExampleParseFeature() {
 	fmt.Fprintf(os.Stdout, "Location: %+v\n", feature.Location)
 	fmt.Fprintf(os.Stdout, "Keyword: %+v\n", feature.Keyword)
 	fmt.Fprintf(os.Stdout, "Name: %+v\n", feature.Name)
-	fmt.Fprintf(os.Stdout, "ScenarioDefinitions: length: %+v\n", len(feature.ScenarioDefinitions))
+	fmt.Fprintf(os.Stdout, "Children: length: %+v\n", len(feature.Children))
 
-	scenario1, _ := feature.ScenarioDefinitions[0].(*gherkin.ScenarioOutline)
+	scenario1, _ := feature.Children[0].(*gherkin.ScenarioOutline)
 	fmt.Fprintf(os.Stdout, " 1: Location: %+v\n", scenario1.Location)
 	fmt.Fprintf(os.Stdout, "    Keyword: %+v\n", scenario1.Keyword)
 	fmt.Fprintf(os.Stdout, "    Name: %+v\n", scenario1.Name)
 	fmt.Fprintf(os.Stdout, "    Steps: length: %+v\n", len(scenario1.Steps))
 
-	scenario2, _ := feature.ScenarioDefinitions[1].(*gherkin.Scenario)
+	scenario2, _ := feature.Children[1].(*gherkin.Scenario)
 	fmt.Fprintf(os.Stdout, " 2: Location: %+v\n", scenario2.Location)
 	fmt.Fprintf(os.Stdout, "    Keyword: %+v\n", scenario2.Keyword)
 	fmt.Fprintf(os.Stdout, "    Name: %+v\n", scenario2.Name)
@@ -57,7 +57,7 @@ func ExampleParseFeature() {
 	// Location: &{Line:1 Column:1}
 	// Keyword: Feature
 	// Name: Tagged Examples
-	// ScenarioDefinitions: length: 2
+	// Children: length: 2
 	//  1: Location: &{Line:3 Column:3}
 	//     Keyword: Scenario Outline
 	//     Name: minimalistic
@@ -88,7 +88,7 @@ func ExampleParseMultipleFeatures() {
 	fmt.Fprintf(os.Stdout, "Location: %+v\n", feature1.Location)
 	fmt.Fprintf(os.Stdout, "Keyword: %+v\n", feature1.Keyword)
 	fmt.Fprintf(os.Stdout, "Name: %+v\n", feature1.Name)
-	fmt.Fprintf(os.Stdout, "ScenarioDefinitions: length: %+v\n", len(feature1.ScenarioDefinitions))
+	fmt.Fprintf(os.Stdout, "Children: length: %+v\n", len(feature1.Children))
 	fmt.Fprintf(os.Stdout, "\n")
 
 	input2 := `Feature: Test2`
@@ -103,7 +103,7 @@ func ExampleParseMultipleFeatures() {
 	fmt.Fprintf(os.Stdout, "Location: %+v\n", feature2.Location)
 	fmt.Fprintf(os.Stdout, "Keyword: %+v\n", feature2.Keyword)
 	fmt.Fprintf(os.Stdout, "Name: %+v\n", feature2.Name)
-	fmt.Fprintf(os.Stdout, "ScenarioDefinitions: length: %+v\n", len(feature2.ScenarioDefinitions))
+	fmt.Fprintf(os.Stdout, "Children: length: %+v\n", len(feature2.Children))
 
 
 	// Output:
@@ -111,12 +111,12 @@ func ExampleParseMultipleFeatures() {
 	// Location: &{Line:1 Column:1}
 	// Keyword: Feature
 	// Name: Test
-	// ScenarioDefinitions: length: 0
+	// Children: length: 0
 	//
 	// Location: &{Line:1 Column:1}
 	// Keyword: Feature
 	// Name: Test2
-	// ScenarioDefinitions: length: 0
+	// Children: length: 0
 	//
 }
 
@@ -155,12 +155,12 @@ Feature: Foo
 		return
 	}
 	feature2 := builder.GetFeature()
-        fmt.Fprintf(os.Stdout, "Comments: length: %+v\n", len(feature2.Comments)) 
+        fmt.Fprintf(os.Stdout, "Comments: length: %+v\n", len(feature2.Comments))
 	fmt.Fprintf(os.Stdout, "Location: %+v\n", feature2.Location)
 	fmt.Fprintf(os.Stdout, "Keyword: %+v\n", feature2.Keyword)
 	fmt.Fprintf(os.Stdout, "Name: %+v\n", feature2.Name)
-	fmt.Fprintf(os.Stdout, "ScenarioDefinitions: length: %+v\n", len(feature2.ScenarioDefinitions))
-	scenario1, _ := feature2.ScenarioDefinitions[0].(*gherkin.Scenario)
+	fmt.Fprintf(os.Stdout, "Children: length: %+v\n", len(feature2.Children))
+	scenario1, _ := feature2.Children[0].(*gherkin.Scenario)
 	fmt.Fprintf(os.Stdout, " 1: Location: %+v\n", scenario1.Location)
 	fmt.Fprintf(os.Stdout, "    Keyword: %+v\n", scenario1.Keyword)
 	fmt.Fprintf(os.Stdout, "    Name: %+v\n", scenario1.Name)
@@ -176,7 +176,7 @@ Feature: Foo
 	// Location: &{Line:1 Column:1}
 	// Keyword: Feature
 	// Name: Foo
-	// ScenarioDefinitions: length: 1
+	// Children: length: 1
 	//  1: Location: &{Line:2 Column:3}
 	//     Keyword: Scenario
 	//     Name: Bar
@@ -202,13 +202,13 @@ func ExampleChangeDefaultDialect() {
 	fmt.Fprintf(os.Stdout, "Location: %+v\n", feature.Location)
 	fmt.Fprintf(os.Stdout, "Keyword: %+v\n", feature.Keyword)
 	fmt.Fprintf(os.Stdout, "Name: %+v\n", feature.Name)
-	fmt.Fprintf(os.Stdout, "ScenarioDefinitions: length: %+v\n", len(feature.ScenarioDefinitions))
+	fmt.Fprintf(os.Stdout, "Children: length: %+v\n", len(feature.Children))
 
 	// Output:
 	//
 	// Location: &{Line:1 Column:1}
 	// Keyword: Egenskap
 	// Name: i18n support
-	// ScenarioDefinitions: length: 0
+	// Children: length: 0
 	//
 }
