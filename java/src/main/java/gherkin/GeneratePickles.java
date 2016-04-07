a7ecda01200f1248af99ea04bf4c4b45db213fc5
Björn Rasmusson <B.Rasmusson@computer.org>
2016-04-07 18:33:22 +0200

    (Java) Allow empty Feature files.

--- java/src/main/java/gherkin/GeneratePickles.java
+++ java/src/main/java/gherkin/GeneratePickles.java
@@ -1,6 +1,6 @@
 package gherkin;
 
-import gherkin.ast.Feature;
+import gherkin.ast.GherkinDocument;
 import gherkin.pickles.Compiler;
 import gherkin.deps.com.google.gson.Gson;
 import gherkin.pickles.Pickle;
@@ -16,7 +16,7 @@ import java.util.List;
 public class GeneratePickles {
     public static void main(String[] args) throws IOException {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        Parser<Feature> parser = new Parser<>(new AstBuilder());
+        Parser<GherkinDocument> parser = new Parser<>(new AstBuilder());
         TokenMatcher matcher = new TokenMatcher();
         Compiler compiler = new Compiler();
         List<Pickle> pickles = new ArrayList<>();
@@ -24,8 +24,8 @@ public class GeneratePickles {
         for (String fileName : args) {
             Reader in = new InputStreamReader(new FileInputStream(fileName), "UTF-8");
             try {
-                Feature feature = parser.parse(in, matcher);
-                pickles.addAll(compiler.compile(feature, fileName));
+                GherkinDocument gherkinDocument = parser.parse(in, matcher);
+                pickles.addAll(compiler.compile(gherkinDocument, fileName));
             } catch (ParserException e) {
                 Stdio.err.println(e.getMessage());
                 System.exit(1);
