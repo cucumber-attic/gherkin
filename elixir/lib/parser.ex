513225effeab12d0ea8d4b61e58016fce1677b79
Julien Biezemans <jb@jbpros.com>
2016-04-21 14:11:12 +0200

    Playing around

--- elixir/lib/parser.ex
+++ elixir/lib/parser.ex
@@ -1,9 +1,21 @@
 # This file is generated. Do not edit! Edit /elixir/parser.elixir.razor instead.
 
 defmodule Gherkin do
+  defmodule Node do
+    defmodule GherkinDocument do
+      defstruct type: "GherkinDocument", feature: nil, comments: []
+    end
+
+    defmodule Feature do
+      defstruct type: "Feature", tags: []
+    end
+  end
+
   defmodule Parser do
     def parse do
-      true
+      %Node.GherkinDocument{
+        feature: %Node.Feature{}
+      }
     end
   end
 end
--- elixir/test/parser_test.exs
+++ elixir/test/parser_test.exs
@@ -3,6 +3,19 @@ defmodule ParserTest do
   doctest Gherkin.Parser
 
   test "parse" do
-    assert Gherkin.Parser.parse() == true
+    assert Gherkin.Parser.parse() == %Gherkin.Node.GherkinDocument{
+      type: "GherkinDocument",
+      feature: %Gherkin.Node.Feature{
+        type: "Feature",
+        tags: [],
+        # location: { line: 1, column: 1 },
+        # language: 'en',
+        # keyword: 'Feature',
+        # name: 'hello',
+        # description: undefined,
+        # children: []
+      },
+      comments: []
+    }
   end
 end
