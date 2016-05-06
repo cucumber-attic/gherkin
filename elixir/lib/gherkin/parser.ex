999e7da214e515a2b4955023d8963262b6e6ebc2
Xavier Defrang <xavier.defrang@gmail.com>
2016-05-06 15:22:36 +0200

    Implement match Feature line

--- elixir/lib/gherkin/parser.ex
+++ elixir/lib/gherkin/parser.ex
@@ -10,29 +10,59 @@ defmodule Gherkin.Parser do
 
   def parse(parser, io) do
     initial_state = %{parser | scanner_state: TokenScanner.new, builder_state: call_builder(parser, :new)}
-    final_state = do_parse(initial_state, io)
+    final_state = parser_loop(initial_state, io)
     #IO.puts "final_state"
     #IO.inspect final_state
     call_builder(parser, :get_result, [final_state.builder_state])
   end
-  defp do_parse(parser, io) do
+  defp parser_loop(parser_state, io) do
     #IO.inspect(parser)
-    {:ok, token, new_scanner_state} = TokenScanner.read(io, parser.scanner_state)
-    new_parser_state = %{parser |
-      scanner_state: new_scanner_state,
-      builder_state: call_builder(parser, :build, [parser.builder_state, token])
-    }
+
+    {:ok, token, new_scanner_state} = TokenScanner.read(io, parser_state.scanner_state)
+
+    #IO.puts "----------------------------------------"
+
     #IO.inspect(token)
-    if Token.eof?(token) do
+
+    matched_token = match_token(parser_state, token)
+
+   # IO.inspect(matched_token)
+
+    new_parser_state = %{parser_state |
+      scanner_state: new_scanner_state,
+      builder_state: call_builder(parser_state, :build, [parser_state.builder_state, matched_token])
+    }
+
+
+    if Token.eof?(matched_token) do
       #IO.puts "out"
       new_parser_state
     else
       #IO.puts "in"
-      do_parse(new_parser_state, io)
+      parser_loop(new_parser_state, io)
     end
   end
 
   defp call_builder(%{builder: builder}, fun, args \\ []) do
     Kernel.apply(builder, fun, args)
   end
+
+  def match_token(_parser_state, token = %{line: nil}) do
+    token
+  end
+  def match_token(parser_state, token = %{line: %{trimmed_line_text: trimmed_line_text}}) do
+    if String.starts_with?(trimmed_line_text, "Feature:") do
+      %{token |
+        matched_type: :FeatureLine,
+        matched_text: trimmed_line_text |> String.slice(8..-1) |> String.strip,
+        matched_keyword: String.slice(trimmed_line_text, 0, 7),
+        matched_indent: token.line.indent,
+        matched_items: [],
+        location: %{token.location | column: token.line.indent + 1},
+      }
+    else
+      token
+    end
+  end
+
 end
