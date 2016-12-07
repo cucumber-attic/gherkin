cedd3dd934738c79f8751b809353c721287c66b9
dobiedad <leomdiv@gmail.com>
2016-12-07 21:00:58 +0000

    Added Georgian support

--- go/dialects_builtin.go
+++ go/dialects_builtin.go
@@ -1,6 +1,6 @@
 package gherkin
 
-// Builtin dialects for af (Afrikaans), am (Armenian), ar (Arabic), ast (Asturian), az (Azerbaijani), bg (Bulgarian), bm (Malay), bs (Bosnian), ca (Catalan), cs (Czech), cy-GB (Welsh), da (Danish), de (German), el (Greek), em (Emoji), en (English), en-Scouse (Scouse), en-au (Australian), en-lol (LOLCAT), en-old (Old English), en-pirate (Pirate), eo (Esperanto), es (Spanish), et (Estonian), fa (Persian), fi (Finnish), fr (French), ga (Irish), gj (Gujarati), gl (Galician), he (Hebrew), hi (Hindi), hr (Croatian), ht (Creole), hu (Hungarian), id (Indonesian), is (Icelandic), it (Italian), ja (Japanese), jv (Javanese), kn (Kannada), ko (Korean), lt (Lithuanian), lu (Luxemburgish), lv (Latvian), mn (Mongolian), nl (Dutch), no (Norwegian), pa (Panjabi), pl (Polish), pt (Portuguese), ro (Romanian), ru (Russian), sk (Slovak), sl (Slovenian), sr-Cyrl (Serbian), sr-Latn (Serbian (Latin)), sv (Swedish), ta (Tamil), th (Thai), tl (Telugu), tlh (Klingon), tr (Turkish), tt (Tatar), uk (Ukrainian), ur (Urdu), uz (Uzbek), vi (Vietnamese), zh-CN (Chinese simplified), zh-TW (Chinese traditional)
+// Builtin dialects for af (Afrikaans), am (Armenian), ar (Arabic), ast (Asturian), az (Azerbaijani), bg (Bulgarian), bm (Malay), bs (Bosnian), ca (Catalan), cs (Czech), cy-GB (Welsh), da (Danish), de (German), el (Greek), em (Emoji), en (English), en-Scouse (Scouse), en-au (Australian), en-lol (LOLCAT), en-old (Old English), en-pirate (Pirate), eo (Esperanto), es (Spanish), et (Estonian), fa (Persian), fi (Finnish), fr (French), ga (Irish), gj (Gujarati), gl (Galician), he (Hebrew), hi (Hindi), hr (Croatian), ht (Creole), hu (Hungarian), id (Indonesian), is (Icelandic), it (Italian), ja (Japanese), jv (Javanese), ka (Georgian), kn (Kannada), ko (Korean), lt (Lithuanian), lu (Luxemburgish), lv (Latvian), mn (Mongolian), nl (Dutch), no (Norwegian), pa (Panjabi), pl (Polish), pt (Portuguese), ro (Romanian), ru (Russian), sk (Slovak), sl (Slovenian), sr-Cyrl (Serbian), sr-Latn (Serbian (Latin)), sv (Swedish), ta (Tamil), th (Thai), tl (Telugu), tlh (Klingon), tr (Turkish), tt (Tatar), uk (Ukrainian), ur (Urdu), uz (Uzbek), vi (Vietnamese), zh-CN (Chinese simplified), zh-TW (Chinese traditional)
 func GherkinDialectsBuildin() GherkinDialectProvider {
 	return buildinDialects
 }
@@ -1729,6 +1729,45 @@ var buildinDialects = gherkinDialectMap{
 				"Tapi ",
 				"Nanging ",
 				"Ananging ",
+			},
+		},
+	},
+	"ka": &GherkinDialect{
+		"ka", "Georgian", "ქართველი", map[string][]string{
+			feature: []string{
+				"თვისება",
+			},
+			background: []string{
+				"კონტექსტი",
+			},
+			scenario: []string{
+				"სცენარის",
+			},
+			scenarioOutline: []string{
+				"სცენარის ნიმუში",
+			},
+			examples: []string{
+				"მაგალითები",
+			},
+			given: []string{
+				"* ",
+				"მოცემული",
+			},
+			when: []string{
+				"* ",
+				"როდესაც",
+			},
+			then: []string{
+				"* ",
+				"მაშინ",
+			},
+			and: []string{
+				"* ",
+				"და",
+			},
+			but: []string{
+				"* ",
+				"მაგ­რამ",
 			},
 		},
 	},
